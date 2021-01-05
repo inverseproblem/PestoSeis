39faee5b9b1c4297dbb23dc123fbc44c2b5159e3
Andrea Zunino <>
2021-01-05 12:23:29 +0100

    namespace cleanup

--- pestoseis/__init__.py
+++ pestoseis/__init__.py
@@ -1,21 +1,11 @@
 
 
 
-
-#from . import seismicwaves2d
-from .seismicwaves2d import gaussource, rickersource
-from .seismicwaves2d import solveacoustic2D,animateacousticwaves,solveelastic2D
-
-
+from . import seismicwaves2d
 
 from . import ttimerays
-from .ttimerays import plotgrid,plotrays,plotttimemod,plotvelmod
-from .ttimerays import setupgrid,rollmod,unrollmod
-from .ttimerays import rayhorlayers,traceall_straight_rays,traceallrays,tracerayhorlay,traveltime
-from .ttimerays import buildtomomat,lininv
 
-
-from .reflectionseismo import fwdconvolve,calcreflectivity,imgshotgath,wiggle,geometrical_spreading,agc
+from . import reflectionseismo
 
 
 # __all__ = ['char', 'rec', 'memmap']
--- pestoseis/ttimerays/__init__.py
+++ pestoseis/ttimerays/__init__.py
@@ -2,9 +2,11 @@
 
 
 
-from .traveltime_rays import *
 
-from .rayhorlayers import *
+from .traveltime_rays import rollmod,unrollmod,setupgrid,lininv,traveltime,traceallrays,traceall_straight_rays,buildtomomat,plotgrid,plotrays,plotvelmod,plotttimemod
+
+
+from .rayhorlayers import tracerayhorlay
   
 
 # __all__ = ["__version__","ttimerays.traveltime_rays"]
