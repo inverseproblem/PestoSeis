67f70a28fa2334eb1a83a9dc98f480ef4806ccb9
Patrick Marty <patrick.marty@erdw.ethz.ch>
2021-05-28 20:17:57 +0200

    Added 1st derivative of ricker stf

--- pestoseis/seismicwaves2d/sourcetimefuncs.py
+++ pestoseis/seismicwaves2d/sourcetimefuncs.py
@@ -1,6 +1,6 @@
 
 
-import numpy as NP
+import numpy as np
 
 #############################################################################
 
@@ -10,8 +10,8 @@ def gaussource( t, t0, f0 ) :
     """
     # boh = f0 .* (t-t0)
     # source = -8.0*boh.*exp( -boh.^2/(4.0*f0)^2 )
-    a = (NP.pi*f0)**2
-    source = - 8.0*a*(t-t0)*NP.exp( -a*(t-t0)**2 )
+    a = (np.pi*f0)**2
+    source = - 8.0*a*(t-t0)*np.exp( -a*(t-t0)**2 )
     
     return source
 
@@ -21,8 +21,15 @@ def rickersource( t, t0, f0 ):
     """
       Ricker wavelet source time function.
     """
-    b = (NP.pi*f0*(t-t0))**2
-    w = (1.0-2.0*b)*NP.exp(-b)
+    b = (np.pi*f0*(t-t0))**2
+    w = (1.0-2.0*b)*np.exp(-b)
     return w
 
 #############################################################################
+
+def ricker_1st_derivative_source( t, t0, f0 ):
+  """
+    First derivative of a wavelet source time function.
+  """
+  source = 2 * np.pi**2 * (t - t0) * f0**2 * np.exp(- np.pi**2 * (t - t0)**2 * f0**2) * (2 * np.pi**2 * (t - t0)**2 * f0**2 - 3)
+  return source
